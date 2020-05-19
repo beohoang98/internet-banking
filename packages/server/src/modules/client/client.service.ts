d1381f3482d9209d356a35345c72c5a5f1712d5a
Beo Hoang <beohoang98@users.noreply.github.com>
2020-05-20 00:00:01 +0700

    fix build

--- packages/server/src/modules/client/client.service.ts
+++ packages/server/src/modules/client/client.service.ts
@@ -1,18 +1,4 @@
 import { Injectable } from "@nestjs/common";
-import { CreateClientCommand } from "@switchit/nestjs-oauth2-server";
-import { CommandBus } from "@nestjs/cqrs";
 
 @Injectable()
-export class ClientService {
-    constructor(private readonly commandBus: CommandBus) {}
-
-    async create(
-        name: string,
-        secret: string,
-        grants: string[],
-        scope: string[],
-    ) {
-        const command = new CreateClientCommand(name, scope.join(","), name);
-        return await this.commandBus.execute(command);
-    }
-}
+export class ClientService {}
