1c099561ff20500903b0a6f767611b1d4f09e0f9
Leonthaibao <leonthaibao@gmail.com>
2020-06-13 20:09:14 +0700

    fix admin role, use guard

--- packages/server/src/modules/admin/admin.controller.ts
+++ packages/server/src/modules/admin/admin.controller.ts
@@ -1,21 +1,33 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    Get,
+    Post,
+    Req,
+    UseGuards,
+    UseInterceptors,
+    ClassSerializerInterceptor,
+} from "@nestjs/common";
 import { ApiBearerAuth, ApiConsumes, ApiTags } from "@nestjs/swagger";
 import { ForRoles } from "@src/guards/role.decorator";
 
 import { AdminRole } from "@src/models/Admin";
 import { CreateAdminDto } from "@src/dto/admin.dto";
 import { AdminService } from "./admin.service";
+import { RoleGuard } from "@src/guards/role.guard";
+import { JwtGuard } from "@src/guards/jwt.guard";
 
 @Controller("admin")
 @ApiTags("admin")
+@UseInterceptors(ClassSerializerInterceptor)
 @ApiBearerAuth()
-//@UseGuards(RoleGuard)
+@UseGuards(RoleGuard, JwtGuard)
 export class AdminController {
     constructor(private readonly adminService: AdminService) {}
 
     @Post("/")
     @ApiConsumes("application/json", "multipart/form-data")
-    @ForRoles(AdminRole.ADMIN, AdminRole.EMPLOYEE)
+    @ForRoles(AdminRole.ADMIN)
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     create(@Body() body: CreateAdminDto) {
         return this.adminService.createEmployee(
